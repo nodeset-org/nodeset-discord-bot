ee54d7d2427be3cd9a8c94a853d808ab5fdc9171
huy <huy@nodeset.io>
2024-11-04 08:56:51 -0800

    Fix test

--- tests/unit/test_event_watcher.py
+++ tests/unit/test_event_watcher.py
@@ -1,6 +1,6 @@
 import os
 from unittest.mock import patch
-from event_watcher import notify_channel
+from event_watcher import EventWatcher
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path='.env.test')
@@ -9,10 +9,13 @@ load_dotenv(dotenv_path='.env.test')
 def test_notify_channel(mock_post):
     """Test the notify_channel function to ensure it sends the correct payload to the webhook."""
 
+    discord_webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
+    watcher = EventWatcher(discord_webhook_url)
+
     message_title = "Test Title"
     message_body = "This is a test message."
 
-    notify_channel(message_title, message_body)
+    watcher.notify_channel(message_title, message_body)
 
     mock_post.assert_called_once()
 
@@ -23,5 +26,5 @@ def test_notify_channel(mock_post):
             "description": message_body,
         }]
     }
-    mock_post.assert_called_with(os.getenv("DISCORD_WEBHOOK_URL"), json=expected_payload)
+    mock_post.assert_called_with(discord_webhook_url, json=expected_payload)
 
